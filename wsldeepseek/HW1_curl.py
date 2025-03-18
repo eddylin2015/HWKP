7385fa0ba2fc4d76ac07257caa75a087459e6176
eddylin2015 <eddylin2015@gmail.com>
2025-03-18 22:17:29 +0800

    chatbox

--- wsldeepseek/HW1_curl.py
+++ wsldeepseek/HW1_curl.py
@@ -2,16 +2,16 @@
 curl http://localhost:11434/api/generate -d '{ "model": "llama3.2", "prompt": "How are you today?"}'
 curl http://localhost:11434/api/generate -d '{ "model": "llama3.2", "prompt": "How are you today?", "stream": false}'"
 """
+
+import requests
+import json
+#response = requests.post(url, json=data)
+#print(response.json())
 url = "http://localhost:11434/api/generate"
 data = {
     "model": "deepseek-r1:8b",
     "prompt": "WHow are you today?"
 }
-import requests
-import json
-#response = requests.post(url, json=data)
-#print(response.json())
-
 response = requests.post(url, json=data, stream=True)
 
 for line in response.iter_lines():
