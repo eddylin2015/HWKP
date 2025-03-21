04b59f78a8a1484ca285bd31b45d1b76d0927f83
eddylin2015 <eddylin2015@gmail.com>
2025-03-21 12:26:36 +0800

    mysql xlsx

--- tutorPy/ex2_pymysql.py
+++ tutorPy/ex2_pymysql.py
@@ -1,1 +1,27 @@
 "pip install pymysql" 
+import pymysql.cursors
+import numpy as np
+import math
+import re
+import datetime
+from pathlib import Path
+import cfg.cfg as cfg
+connection = pymysql.connect(host=cfg.mysql_host,
+                             user=cfg.mysql_user,
+                             password=cfg.mysql_password,
+                             port=3308,
+                             db=cfg.mysql_db,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+f=["insert studtran (STUD_REF,GRADE,CLASS,C_NO) values "]
+try:
+    with connection.cursor() as cursor:
+        sql="select STUD_REF from studmain;"
+        cursor.execute(sql,())
+        result=cursor.fetchall()
+        for idx,row in enumerate(result):
+            f.append(f"('{row["STUD_REF"]}','**','*',0)")
+finally:
+    connection.close()
+p=Path('out.txt')
+p.write_text(',\r\n'.join(f),encoding='utf8')
--- tutorPy/ex3_openxl.py
+++ tutorPy/ex3_openxl.py
@@ -1,1 +1,23 @@
 "pip install openpyxl" 
+import numpy as np
+from IPython.display import Latex,HTML,Markdown
+import pandas as pd
+import re
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from pathlib import Path
+import datetime
+wb=load_workbook(filename='table.xlsx')
+wb.sheetnames
+mcol=5; mrow=2827
+ws=wb['工作表4']
+data=[]
+data_=[]
+for idx,row in enumerate(ws.iter_rows(min_row=0,max_col=mcol,max_row=mrow)):
+    r1=[]
+    for i in range(0,mcol):
+        v=row[i].value
+        r1.append(v)
+    data.append(r1)
+
+print(data)
